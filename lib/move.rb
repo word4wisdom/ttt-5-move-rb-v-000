c491ebb7e8208abb90a5deb3ea7efdf6ddd588b1
Joan' Araujo <wisdombthegame@gmail.com>
2020-01-31 01:18:04 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,18 +1,4 @@
-board = [" "," ", " ", " "," "," "," "," "," ",]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end   
-
-def input_to_index(input)
-  input.to_i - 1 
-end
-
-def move(board, input, value = "X")
-board[input] = value
-end
+  def move(board, position, character = "X")
+  end
