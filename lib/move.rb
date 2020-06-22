c3070e874e2dfddbd944d1572b991887a92747db
Joan' Araujo <wisdombthegame@gmail.com>
2020-06-22 18:37:15 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -4,8 +4,8 @@ ef display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 end
-
 # code your input_to_index and move method here!
 
 def input_to_index(input)
